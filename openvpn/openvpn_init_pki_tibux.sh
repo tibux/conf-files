b5be100095c7cd3cde153fdabbc087016b6d498d
tibux <tib@tibux.org>
2013-03-31 14:30:32 +0200

    Fix typo

--- openvpn/openvpn_init_pki_tibux.sh
+++ openvpn/openvpn_init_pki_tibux.sh
@@ -2,9 +2,7 @@
 #
 # openvpn : init pki
 
-set -e
-#set -v
-#set -x
+set -vxe
 
 EASY_RSA_PATH="/usr/doc/openvpn-*/easy-rsa/2.0"
 
@@ -16,25 +14,25 @@ CLIENTS="farnese gutz isidro casca skullknight"
 
   source ./vars
 
-  export KEY_SIZE=2048
+  export KEY_SIZE="2048"
+  export KEY_CN=""
 
   export KEY_COUNTRY="FR"
   export KEY_PROVINCE="IleDeFrance"
   export KEY_CITY="Paris"
   export KEY_ORG="Tibux"
-  export KEY_EMAIL="tib@tibux.org"
-  export KEY_CN=""
-  export KEY_NAME="Thibaut Notteboom"
+  export KEY_EMAIL="jean@tibux.org"
+  export KEY_NAME="Jean Cloud"
   export KEY_OU="vpn"
 
   ./clean-all
-  ./build-dh 2>&1 >/dev/null
+  ./build-dh
 
   echo "Creation du certicat CA"
   ./pkitool --initca 2>&1 >/dev/null
 
   echo "Creation du certificat serveur"
-  ./pkitool --server serpico 2>&1 >/dev/null
+  ./pkitool --server $SERVER 2>&1 >/dev/null
 
   echo "Creation des certificats clients..."
   for c in $CLIENTS; do ./pkitool $c 2>&1 >/dev/null ; done
